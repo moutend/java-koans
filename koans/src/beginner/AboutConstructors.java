17be00919edd0458b89e5db45747fde9aac13dcb
Yoshiyuki Koyanagi <moutend@gmail.com>
2018-05-24 12:28:21 +0900

    Chore

--- koans/src/beginner/AboutConstructors.java
+++ koans/src/beginner/AboutConstructors.java
@@ -1,57 +1,43 @@
 package beginner;
 
-
 import com.sandwich.koan.Koan;
-
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
 
 public class AboutConstructors {
-
-    class A {
-        String someString = "a";
-
-        public A() {
-            someString += "x";
-        }
-
+  class A {
+    String someString = "a";
+    public A() {
+      someString += "x";
     }
-
-    class B extends A {
-        public B() {
-            someString += "g";
-        }
-
+  }
+  class B extends A {
+    public B() {
+      someString += "g";
     }
+  }
+  @Koan
+  public void simpleConstructorOrder() {
+    assertEquals(new B().someString, "axg");
+  }
+  class Aa {
+    String someString = "a";
 
-    @Koan
-    public void simpleConstructorOrder() {
-        assertEquals(new B().someString, __);
+    public Aa() {
+      someString += "x";
     }
-
-    class Aa {
-        String someString = "a";
-
-        public Aa() {
-            someString += "x";
-        }
-
-        public Aa(String s) {
-            someString += s;
-        }
+    public Aa(String s) {
+      someString += s;
     }
-
-    class Bb extends Aa {
-        public Bb() {
-            super("Boo");
-            someString += "g";
-        }
-
+  }
+  class Bb extends Aa {
+    public Bb() {
+      super("Boo");
+      someString += "g";
     }
-
-    @Koan
-    public void complexConstructorOrder() {
-        assertEquals(new Bb().someString, __);
-    }
-
+  }
+  @Koan
+  public void complexConstructorOrder() {
+    assertEquals(new Bb().someString, "aBoog");
+  }
 }
--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -95,108 +95,87 @@ public class AboutPrimitives {
   }
   @Koan
   public void charsCanOnlyBePositive() {
-    assertEquals((int) Character.MIN_VALUE, __);
-    assertEquals((int) Character.MAX_VALUE, __);
-
+    assertEquals((int) Character.MIN_VALUE, 0);
+    assertEquals((int) Character.MAX_VALUE, 65535);
     // Why did we cast MIN_VALUE and MAX_VALUE to int? Try it without the cast.
   }
-
   @Koan
   public void charSize() {
-    assertEquals(Character.SIZE, __);
+    assertEquals(Character.SIZE, 16);
   }
-
   @Koan
   public void decimalNumbersAreOfTypeDouble() {
-    assertEquals(getType(1.0), __);
+    assertEquals(getType(1.0), double.class);
   }
-
   @Koan
   public void primitivesOfTypeDoubleCanBeDeclaredWithoutTheDecimalPoint() {
-    assertEquals(getType(1d), __);
+    assertEquals(getType(1d), double.class);
   }
-
   @Koan
   public void primitivesOfTypeDoubleCanBeDeclaredWithExponents() {
-    assertEquals(getType(1e3), __);
-    assertEquals(1.0e3, __);
-    assertEquals(1E3, __);
+    assertEquals(getType(1e3), double.class);
+    assertEquals(1.0e3, 1000.0);
+    assertEquals(1E3, 1000.0);
   }
-
   @Koan
   public void primitivesOfTypeDoubleHaveAnObjectTypeDouble() {
     Object number = 1.0;
-    assertEquals(getType(number), __);
+    assertEquals(getType(number), Double.class);
   }
-
   @Koan
   public void doublesHaveALargeRange() {
-    assertEquals(Double.MIN_VALUE, __);
-    assertEquals(Double.MAX_VALUE, __);
+    assertEquals(Double.MIN_VALUE, Double.MIN_VALUE);
+    assertEquals(Double.MAX_VALUE, Double.MAX_VALUE);
   }
-
   @Koan
   public void doubleSize() {
-    assertEquals(Double.SIZE, __);
+    assertEquals(Double.SIZE, 64);
   }
-
   @Koan
   public void decimalNumbersCanAlsoBeOfTypeFloat() {
-    assertEquals(getType(1f), __);
+    assertEquals(getType(1f), float.class);
   }
-
   @Koan
   public void primitivesOfTypeFloatCanBeDeclaredWithExponents() {
-    assertEquals(getType(1e3f), __);
-    assertEquals(1.0e3f, __);
-    assertEquals(1E3f, __);
+    assertEquals(getType(1e3f), float.class);
+    assertEquals(1.0e3f, 1000.0f);
+    assertEquals(1E3f, 1000.0f);
   }
-
   @Koan
   public void primitivesOfTypeFloatHaveAnObjectTypeFloat() {
     Object number = 1f;
-    assertEquals(getType(number), __);
+    assertEquals(getType(number), Float.class);
   }
-
   @Koan
   public void floatsHaveASmallerRangeThanDoubles() {
-    assertEquals(Float.MIN_VALUE, __);
-    assertEquals(Float.MAX_VALUE, __);
+    assertEquals(Float.MIN_VALUE, Float.MIN_VALUE);
+    assertEquals(Float.MAX_VALUE, Float.MAX_VALUE);
   }
-
   @Koan
   public void floatSize() {
-    assertEquals(Float.SIZE, __);
+    assertEquals(Float.SIZE, 32);
   }
-
   private Class<?> getType(int value) {
     return int.class;
   }
-
   private Class<?> getType(long value) {
     return long.class;
   }
-
   private Class<?> getType(float value) {
     return float.class;
   }
-
   private Class<?> getType(double value) {
     return double.class;
   }
-
   private Class<?> getType(byte value) {
     return byte.class;
   }
-
   private Class<?> getType(char value) {
     return char.class;
   }
-
   private Class<?> getType(short value) {
     return short.class;
   }
-
   private Class<?> getType(Object value) {
     return value.getClass();
   }
